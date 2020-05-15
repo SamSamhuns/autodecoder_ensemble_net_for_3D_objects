16c4be61d1b7559edaddfbba6a6c7440f1ad4384
SamSamhuns <samhunsadamant@gmail.com>
2020-05-15 17:46:11 +0400

    Add separated comps

--- src/utils.py
+++ src/utils.py
@@ -48,6 +48,46 @@ def visualize_npy(npy_3d_matrix, save_img_fpath='./img/npy_01.png', remove_ticks
     if save_img_fpath is not None:
         plt.savefig(save_img_fpath)
 
+def find_encoding(X, y, autodecoder, encoding_iters=300,
+                  encoding_size=256, lr=5e-4, l2_reg=False):
+    """
+    Generate the encoding (latent vector) for each data in X
+    """
+
+    def _adjust_lr(initial_lr, optimizer, num_iters, decreased_by, adjust_lr_every):
+
+        lr = initial_lr * ((1 / decreased_by) **
+                           (num_iters // adjust_lr_every))
+        for param_group in optimizer.param_groups:
+            param_group["lr"] = lr
+
+    decreased_by = 10
+    adjust_lr_every = encoding_iters // 2
+
+    encoding = torch.ones(X.shape[0], encoding_size).normal_(
+        mean=0, std=1.0 / math.sqrt(encoding_size)).cuda()
+
+    encoding.requires_grad = True
+    optimizer = torch.optim.Adam([encoding], lr=lr)
+    loss_num = 0
+
+    for i in range(encoding_iters):
+        autodecoder.eval()
+        _adjust_lr(lr, optimizer, i, decreased_by, adjust_lr_every)
+        optimizer.zero_grad()
+        y_pred = autodecoder(X, encoding)
+        loss = chamfer_loss(y_pred, y, ps=y.shape[-1])
+
+        if l2_reg:
+            loss += 1e-4 * torch.mean(encoding.pow(2))
+        loss.backward()
+        optimizer.step()
+
+        if i % 50 == 0:
+            print(i, loss.cpu().data.numpy(), encoding.norm())
+        loss_num = loss.cpu().data.numpy()
+
+    return loss_num, encoding
         
 def print_model_metrics(total_loss, 
                         same_corr_cnt, 
