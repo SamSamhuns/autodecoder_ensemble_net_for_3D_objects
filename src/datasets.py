56291dc29c9baf832522ccf627f1e4035b689950
SamSamhuns <samhunsadamant@gmail.com>
2020-05-15 14:17:53 +0400

    Add datasets & models

---
+++ src/datasets.py
@@ -0,0 +1,108 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+
+from torch.autograd import Variable
+from torch.utils.data import Dataset, DataLoader
+
+import random
+import numpy as np
+
+
+class PointNetDS(Dataset):
+    """
+    Create train dataset
+    """
+
+    def __init__(self, data, sampling_interval=3):
+        # sample every sampling_interval-th point to speed up
+        self.data = data.transpose((0, 2, 1))[:, :, ::sampling_interval]
+        self.data = torch.from_numpy(self.data).float()
+
+    def __len__(self):
+        return self.data.shape[0]
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+
+class PointDriftDS(Dataset):
+    """
+    Pairs each shape with one shape from the same class and one shape from a different class
+    """
+
+    def __init__(self, data, labels, sampling_interval=3):
+        # sample every sampling_interval-th point to speed up
+        self.data = data.transpose((0, 2, 1))[:, :, ::sampling_interval]
+        self.labels = labels.squeeze()
+
+        self.same_cls = []
+        self.diff_cls = []
+        idx_arr = np.arange(self.data.shape[0])
+        same_idx = []
+        diff_idx = []
+        for i in range(self.labels.max() + 1):
+            same_idx.append(idx_arr[self.labels == i])
+            diff_idx.append(idx_arr[self.labels != i])
+        for i in range(data.shape[0]):
+            same = same_idx[self.labels[i]]
+            diff = diff_idx[self.labels[i]]
+            self.same_cls.append(same[random.randint(0, len(same) - 1)])
+            self.diff_cls.append(diff[random.randint(0, len(diff) - 1)])
+        self.data = torch.from_numpy(self.data).float()
+
+    def __len__(self):
+        return self.data.shape[0]
+
+    def __getitem__(self, idx):
+        X = self.data[idx]
+        same_cls_data = self.data[self.same_cls[idx]]
+        diff_cls_data = self.data[self.diff_cls[idx]]
+
+        return X, same_cls_data, diff_cls_data, idx
+
+
+class EncodingDS(Dataset):
+    """
+    Generate encoding for each pair of shapes in the PointDriftDS
+    """
+
+    def __init__(self, PDDS, autodecoder, latent_size=256):
+        self.PointDriftDS = PDDS
+        self.autodecoder = autodecoder
+        self.latent_size = latent_size
+        self.same_cls = torch.zeros((len(self.PointDriftDS), latent_size))
+        self.diff_cls = torch.zeros((len(self.PointDriftDS), latent_size))
+
+    def train_encodings(self, num_iterations=50, lr=0.01, l2_reg=False, batch_size=16):
+        dl = DataLoader(self.PointDriftDS,
+                        batch_size=batch_size, shuffle=False)
+        i = 0
+        batch_cnt = 0
+        same_cls_loss = 0.0
+        diff_cls_loss = 0.0
+        self.autodecoder.eval()
+
+        for batch_idx, (x, y, z, idx) in enumerate(dl):
+            j = i + len(idx)
+            loss, encoding = find_encoding(x, y, self.autodecoder, encoding_iters=num_iterations,
+                                           encoding_size=self.latent_size, lr=lr, l2_reg=l2_reg,)
+            same_cls_loss += loss
+            self.same_cls[i:j] = encoding
+            loss, encoding = find_encoding(x, z, self.autodecoder, encoding_iters=num_iterations,
+                                           encoding_size=self.latent_size, lr=lr, l2_reg=l2_reg,)
+            diff_cls_loss += loss
+            self.diff_cls[i:j] = encoding
+
+            i = j
+            batch_cnt += 1
+        print("Encodings trained")
+        return (self.same_cls, self.diff_cls, same_cls_loss / batch_cnt, diff_cls_loss / batch_cnt)
+
+    def __len__(self):
+        return len(self.PointDriftDS)
+
+    def __getitem__(self, idx):
+        return (*self.PointDriftDS[idx], self.same_cls[idx], self.diff_cls[idx])
---
+++ src/models.py
@@ -0,0 +1,119 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+
+from torch.autograd import Variable
+from torch.utils.data import Dataset, DataLoader
+
+import random
+import numpy as np
+
+
+class AutoDecoder(nn.Module):
+    """
+    AutoDecoder NN to learn point drift (latent encoding) between two 3D shapes
+    """
+
+    def __init__(self,  encoding_dim=256, point_dim=3):
+        super(AutoDecoder, self).__init__()
+        self.fc1 = nn.Conv1d(encoding_dim + point_dim, 128, 1)
+        self.fc2 = nn.Conv1d(128, 64, 1)
+        self.fc3 = nn.Conv1d(64, point_dim, 1)
+
+    def forward(self, X, encoding):
+        num_points = X.shape[-1]  # num of points in each shape
+        enc = encoding.unsqueeze(-1).repeat(1, 1, num_points)
+        X_enc = torch.cat([X, enc], 1)
+        X_enc = F.leaky_relu(self.fc1(X_enc))
+        X_enc = F.leaky_relu(self.fc2(X_enc))
+
+        # Return the drift from obj X determined by the latent encoding
+        return X + self.fc3(X_enc)
+
+
+class EnsembleAutoDecoder(nn.Module):
+    """
+    Ingests the latent encoding of two 3D objects
+    and outputs the similarity score using an ensemble of CompNets
+    Stacked Ensemble
+    """
+
+    def __init__(self, adnet_list=ensemble_adnet, encoding_size=256, point_dim=3):
+        """
+        if comp_net is a module, EnsembleCompNet creates num_ensemble*comp_net NN modules
+        if comp_net is a list of modules, EnsembleCompNet iterates through comp_net to get the NN modules
+        """
+        super(EnsembleAutoDecoder, self).__init__()
+        self.ensemble_adnet = nn.ModuleList()
+
+        for adnet in adnet_list:
+            self.ensemble_adnet.append(adnet)
+
+        self.fc_final = nn.Conv1d(64 * len(adnet_list), point_dim, 1)
+
+    def forward(self, X, encoding):
+        num_points = X.shape[-1]  # num of points in each shape
+        enc = encoding.unsqueeze(-1).repeat(1, 1, num_points)
+
+        X_enc = torch.cat([X, enc], 1)
+        enc_output_list = [net(X_enc) for net in self.ensemble_adnet]
+
+        # Return the drift from obj X determined by the latent encoding
+        return X + self.fc_final(torch.cat(enc_output_list, dim=1))
+
+
+class CompNet(nn.Module):
+    """
+    Ingests the latent encoding of two 3D objects
+    and outputs the similarity score
+    """
+
+    def __init__(self, encoding_size=256):
+        super(CompNet, self).__init__()
+        self.fc1 = nn.Linear(encoding_size, 64)
+        self.fc2 = nn.Linear(64, 1)
+
+    def forward(self, encoding):
+        X = F.leaky_relu(self.fc1(encoding))
+        return torch.sigmoid(self.fc2(X))
+
+
+class EnsembleCompNet(nn.Module):
+    """
+    Ingests the latent encoding of two 3D objects
+    and outputs the similarity score using an ensemble of CompNets
+    Stacked Ensemble
+    """
+
+    def __init__(self, comp_net=CompNet, num_ensemble=5, encoding_dim=256, seed_val=SEED):
+        """
+        if comp_net is a module, EnsembleCompNet creates num_ensemble*comp_net NN modules
+        if comp_net is a list of modules, EnsembleCompNet iterates through comp_net to get the NN modules
+        """
+        super(EnsembleCompNet, self).__init__()
+        self.ensemble_compnet = nn.ModuleList()
+
+        if isinstance(comp_net, list):
+            if num_ensemble != len(comp_net):
+                raise IndexError(
+                    f"Length of comp_nets: {len(comp_net)} and num_ensemble: {num_ensemble} do not match")
+            comp_net_list = comp_net
+            for i in range(num_ensemble):
+                self.ensemble_compnet.append(comp_net_list[i])
+        else:
+            for i in range(num_ensemble):
+                torch.manual_seed(seed_val * i + 1)
+                if use_cuda:
+                    torch.cuda.manual_seed(seed_val * i + 1)
+                self.ensemble_compnet.append(comp_net(encoding_dim))
+        self.final = nn.Linear(num_ensemble, 1)
+
+    def forward(self, encoding):
+        """ Returns the final value of the results after a nn.Linear layer """
+        total_pred = torch.cat([net(encoding)
+                                for net in self.ensemble_compnet])
+        total_pred = total_pred.reshape(-1, len(self.ensemble_compnet))
+
+        return torch.sigmoid(self.final(total_pred))
